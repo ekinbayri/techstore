f2f7eb07e6dc702339c66b706819e032083b78da
ekinbayri <ekinbayri@gmail.com>
2022-12-04 17:39:04 +0300

    Changed to page structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 
 import './App.css';
-import Header from './components/Header';
-import Items from './components/Items';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import CategoryElement from './components/CategoryElement';
+import Index from './pages/index.js'
 function App() {
   return (
-    <Container fluid = "true">
-      <Row>
-        <Col><Header /></Col>
-      </Row>
-      <Row>
-        <Col><CategoryElement /></Col>
-      </Row>
-      <Row>
-        <Col className = "center"> <Items /> </Col>
-      </Row>
-      
-    </Container>
-    
+   
+    <Index />
   
    
   );
---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Header from '../components/Header';
+import Items from '../components/Items';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import CategoryElement from '../components/CategoryElement';
+
+function Index() {
+    return (
+<Container fluid = "true">
+    <Row>
+        <Col><Header /></Col>
+    </Row>
+    <Row>
+        <Col><CategoryElement /></Col>
+    </Row>
+    <Row>
+        <Col className = "center"> <Items /> </Col>
+    </Row>
+
+</Container>
+     
+    );
+  }
+export default Index;
