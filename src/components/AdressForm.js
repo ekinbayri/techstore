49ad2739a812ba573060e4ec8a0d0c8a7ad53e99
ekinbayri <ekinbayri@gmail.com>
2022-12-29 18:23:28 +0300

    addressform

---
+++ src/components/AdressForm.js
@@ -0,0 +1,92 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+function AdressForm(){
+    const [addressExplanation,setAddressExplanation] = useState("")
+    const [city,setCity] = useState("")
+    const [district,setDistrict] = useState("")
+    const [street,setStreet] = useState("")
+
+    let navigate = useNavigate();
+    const handleAddressExplanation = (value) =>{
+        setAddressExplanation(value)
+    }
+   
+    const handleCity = (value) =>{
+        setCity(value)
+    }
+    const handleDistrict = (value) =>{
+        setDistrict(value)
+    }
+    const handleStreet = (value) =>{
+        setStreet(value)
+    }
+    const handleForm = () =>{
+        sendRequest()
+        setAddressExplanation("")
+        setCity("")
+        setDistrict("")
+        setStreet("")
+        
+
+    }
+    const sendRequest = () =>{
+        fetch("http://localhost:8080/address",{
+            method : "POST",
+            headers : {
+                "Authorization": localStorage.getItem("tokenKey"),
+                "Content-Type":"application/json"
+            },
+            body : JSON.stringify({
+                city : city,
+                district: district,
+                street: street,
+                addressExplanation: addressExplanation,
+
+            }),
+        })
+        .then((res) => res.json())
+        .then((result) =>  {console.log(result);
+                         })
+
+        .catch((err) => console.log(err))
+    }
+    return (
+        <Form>
+    
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" onChange={(e) => handleAddressExplanation(e.target.value)}/>
+      </Form.Group>
+
+
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control onChange={(e) => handleCity(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>District</Form.Label>
+          <Form.Control onChange={(e) => handleDistrict(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Street</Form.Label>
+          <Form.Control onChange={(e) => handleStreet(e.target.value)}/>
+        </Form.Group>
+      </Row>
+
+     
+      <Button variant="primary" onClick={handleForm}>
+        Submit
+      </Button>
+    </Form>
+         
+      );
+    }
+    
+    export default AdressForm;
