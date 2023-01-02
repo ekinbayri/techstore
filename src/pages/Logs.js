c19b7207ea7a443339e87a615dc8fd3705a32d2f
mcany <83024513+hartigan8@users.noreply.github.com>
2023-01-02 19:27:08 +0300

    logs added

---
+++ src/pages/Logs.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Table } from 'react-bootstrap'
+
+function Logs() {
+    const [logs, setLogs] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:8080/logs/",{
+            method : "GET",
+            headers : {
+                "Authorization": localStorage.getItem("tokenKey"),
+                "Content-Type":"application/json"
+            },
+           
+        })
+          .then((res) => res.json())
+          .then((result) =>  setLogs(result))
+          .catch((err) => console.log(err))
+      
+    }, [])
+    
+  return (
+    <Table>
+
+      <thead>
+        <tr>
+          <th>sessionId</th>
+          <th>sessionLineNum</th>
+          <th>logTime</th>
+          <th>userName</th>
+          <th>databaseName</th>
+          <th>processId</th>
+          <th>connectionFrom</th>
+          <th>commandTag</th>
+          <th>sessionStartTime</th>
+          <th>virtualTransactionId</th>
+          <th>transactionId</th>
+          <th>errorSeverity</th>
+          <th>sqlStateCode</th>
+          <th>message</th>
+          <th>detail</th>
+          <th>hint</th>
+          <th>internalQuery</th>
+          <th>internalQueryPos</th>
+          <th>context</th>
+          <th>query</th>
+          <th>queryPos</th>
+          <th>location</th>
+          <th>applicationName</th>
+          <th>backendType</th>
+          <th>leaderPid</th>
+          <th>queryId</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {logs.map(log => <Log log = {log} ></Log>)}
+      </tbody>
+      
+    </Table>
+  )
+}
+
+export default Logs
